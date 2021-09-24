9178f5fc814f8be617dc35ce20e9b547ed8d8ef9
StefanNijssen <58977715+StefanNijssen@users.noreply.github.com>
2021-09-24 11:41:10 +0200

    commit2

--- Ijswinkel.py
+++ Ijswinkel.py
@@ -1,12 +1,23 @@
 print('Welkom bij Papi Gelato je mag alle smaken kiezen zolang het maar vanille ijs is.')
-
+def soort_bolletje(aantal):
+    x = 1
+    for i in range(1,aantal+1):
+        soort = input('Welke smaak wilt u voor bolletje nummer' + str(x) + '? A) Aardbei, C) Chocolade, M) Munt of V) Vanille?')
+        if soort == "V" or "M" or "C" or "A":
+            x += 1
+        else:
+            print('Sorry dat snap ik niet...')
+            soort_bolletje()
 
 def aantal_bolletjes():
     aantal = int(input('Hoeveel bolletjes wilt u?'))
     if aantal >= 1 and aantal <= 3:
+        soort_bolletje(aantal)
         bakje_hoorntje(aantal)
+
     elif aantal >= 4 and aantal <= 8:
         print('Dan krijgt u van mij een bakje met ' + str(aantal) + ' bolletjes')
+        soort_bolletje(aantal)
     elif aantal > 8:
         print('Sorry, zulke grote bakken hebben we niet')
     else:
